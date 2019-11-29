bd642f17070f7f8e6a029b893541c7b9a54b2878
Marc <mr.moreaux@gmail.com>
2019-11-29 20:57:16 +0100

    Support loading a model on cuda and cpu

--- model.py
+++ model.py
@@ -110,44 +110,30 @@ def generate_model(opt):
         elif opt.model_depth == 264:
             model = densenet.densenet264(**args)
 
-    if not opt.no_cuda:
-        model = model.cuda()
+    if opt.no_cuda:
+        device = 'cpu'
+    else:
+        device = 'cuda'
+        model = model.to(device)
         model = nn.DataParallel(model, device_ids=None)
 
-        if opt.pretrain_path:
-            print('loading pretrained model {}'.format(opt.pretrain_path))
-            pretrain = torch.load(opt.pretrain_path)
-            assert opt.arch == pretrain['arch']
+    if opt.pretrain_path:
+        print('loading pretrained model {}'.format(opt.pretrain_path))
+        pretrain = torch.load(opt.pretrain_path, map_location=device)
+        assert opt.arch == pretrain['arch']
 
-            model.load_state_dict(pretrain['state_dict'])
+        model.load_state_dict(pretrain['state_dict'])
 
-            if opt.model == 'densenet':
-                model.module.classifier = nn.Linear(
-                    model.module.classifier.in_features, opt.n_finetune_classes)
-                model.module.classifier = model.module.classifier.cuda()
-            else:
-                model.module.fc = nn.Linear(model.module.fc.in_features,
-                                            opt.n_finetune_classes)
-                model.module.fc = model.module.fc.cuda()
+        if opt.model == 'densenet':
+            model.module.classifier = nn.Linear(
+                model.module.classifier.in_features, opt.n_finetune_classes)
+            model.module.classifier = model.module.classifier.to(device)
+        else:
+            model.module.fc = nn.Linear(model.module.fc.in_features,
+                                        opt.n_finetune_classes)
+            model.module.fc = model.module.fc.to(device)
 
-            parameters = get_fine_tuning_parameters(model, opt.ft_begin_index)
-            return model, parameters
-    else:
-        if opt.pretrain_path:
-            print('loading pretrained model {}'.format(opt.pretrain_path))
-            pretrain = torch.load(opt.pretrain_path)
-            assert opt.arch == pretrain['arch']
-
-            model.load_state_dict(pretrain['state_dict'])
-
-            if opt.model == 'densenet':
-                model.classifier = nn.Linear(
-                    model.classifier.in_features, opt.n_finetune_classes)
-            else:
-                model.fc = nn.Linear(model.fc.in_features,
-                                     opt.n_finetune_classes)
-
-            parameters = get_fine_tuning_parameters(model, opt.ft_begin_index)
-            return model, parameters
+        parameters = get_fine_tuning_parameters(model, opt.ft_begin_index)
+        return model, parameters
 
     return model, model.parameters()
